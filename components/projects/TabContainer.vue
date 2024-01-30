bedb880f926e3363c8f4767d15f82b5bb57061ea
junaidsahar <imaadahmed300@gmail.com>
2024-01-30 10:32:47 -0800

    project page design

---
+++ components/projects/TabContainer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="max-w-7xl mx-auto px-5 mb-16">
+    <div
+      class="max-w-5xl mx-auto p-5 navbar flex lg:justify-between gap-2 flex-wrap md:flex-row flex-col"
+    >
+      <button
+        class="navbar px-6 py-4 text-sm font-semibold"
+        v-for="categories in [
+          'UI UX Projects',
+          'Web Design',
+          'Web Design',
+          'Cloud Computing',
+          'Cyber Security',
+          'Software Development',
+        ]"
+      >
+        {{ categories }}
+      </button>
+    </div>
+    <div class="grid md:grid-cols-2 gap-10 mt-14">
+      <div v-for="x in 4">
+        <img src="@/assets/images/dummy-project-image.png" alt="project image" class="w-full" />
+        <div class="pt-5 flex justify-between">
+          <p class="font-semibold text-2xl">AL-Nafi Cloud</p>
+          <button class="bg-background text-foreground px-3 py-2 rounded-full border">
+            ui ux
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/projects.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <CenteredHeroSection
+      :tagline="heroSectionData.tagline"
+      :title="heroSectionData.title"
+      :punchline="heroSectionData.punchline"
+    />
+    <HomeServicesCrossSlider />
+    <LazyProjectsTabContainer />
+  </div>
+</template>
+
+<script setup>
+const heroSectionData = {
+  tagline: "Plan | Design | Develop | Test | Deploy",
+  title: {
+    type: "h1",
+    contentArray: [
+      {
+        text: "Take a look at our most ",
+        type: "text",
+        class: "",
+      },
+
+      {
+        text: "successful projects",
+        type: "text",
+        class: "text-foregroundPrimary",
+      },
+    ],
+  },
+  punchline: {
+    type: "p",
+    contentArray: [
+      {
+        text: "Our comprehensive suite of services is designed to empower your venture and drive success",
+        type: "text",
+        class: "",
+      },
+      {
+        text: "",
+        type: "breakpoint",
+        class: "",
+      },
+      {
+        text: "Schedule a call and explore how we can bring your vision to life",
+        type: "text",
+        class: "",
+      },
+    ],
+  },
+};
+</script>
