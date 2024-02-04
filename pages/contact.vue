49f9e1458c036e922c0d40a240c9155e496e5b5a
junaidsahar <imaadahmed300@gmail.com>
2024-02-04 14:30:31 +0500

    contact form api

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="relative">
-    <div class="max-w-7xl mx-auto pb-10 flex items-center lg:flex-row flex-col gap-12 px-5 pt-6">
+    <div
+      class="max-w-7xl mx-auto pb-10 flex items-center lg:flex-row flex-col gap-12 px-5 pt-6"
+    >
       <div class="lg:max-w-[500px] w-full space-y-3">
         <h1 class="text-6xl font-semibold">Contacts</h1>
         <h4 class="text-2xl text-foregroundPrimary">Get in Touch</h4>
@@ -24,7 +26,10 @@
         </div>
       </div>
       <div class="lg:flex-1 w-full">
-        <form class="md:max-w-xl w-full lg:ml-auto p-7 static-shadow-border space-y-4">
+        <form
+          @submit.prevent="submitForm"
+          class="md:max-w-xl w-full lg:ml-auto p-7 static-shadow-border space-y-4"
+        >
           <div
             class="flex flex-col gap-2"
             v-for="(input, index) in inputData"
@@ -33,16 +38,22 @@
             <label class="text-lg font-semibold">{{ input.label }}</label>
             <textarea
               v-if="input.name == 'message'"
+              v-model="input.value"
               class="p-2 border rounded-lg border-black relative"
               rows="4"
             ></textarea>
             <input
               v-else
+              v-model="input.value"
               type="text"
               class="py-4 px-2 border rounded-lg border-black"
             />
           </div>
-          <ButtonPrimary button-text="Submit" />
+          <Button
+            type="submit"
+            class="block text-white font-semibold px-3 py-2 sm:px-4 sm:py-3 md:px-6 md:py-4 bg-foregroundPrimary hover:bg-foregroundPrimaryHover rounded-2xl border-black border-2 transition-all"
+            >Submit</Button
+          >
         </form>
       </div>
     </div>
@@ -82,4 +93,19 @@ const inputData = ref([
     value: "",
   },
 ]);
+
+const submitForm = async () => {
+  const { data: responseData } = await useFetch("api/email-sender", {
+    method: "post",
+    body: {
+      first_name: inputData.value[0].value,
+      last_name: inputData.value[1].value,
+      email: inputData.value[2].value,
+      phone: inputData.value[3].value,
+      message: inputData.value[4].value,
+    },
+  });
+
+  console.log(responseData.value);
+};
 </script>
