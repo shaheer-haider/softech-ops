607469e98d5587c56a0e9d0c5f064d78ba32d2a3
shaheer-haider <shaheerhaider00000@gmail.com>
2024-02-12 14:11:15 +0500

    more fixes

--- components/projects/TabContainer.vue
+++ components/projects/TabContainer.vue
@@ -1,48 +1,83 @@
 <template>
   <div class="max-w-7xl mx-auto px-5 mb-4">
     <div
-      class="max-w-5xl mx-auto p-5 static-shadow-border-light flex justify-start gap-2 flex-wrap md:flex-row flex-col bg-foreground"
+      class="max-w-5xl mx-auto p-5 static-shadow-border-light flex justify-center gap-y-2 gap-x-3 flex-wrap md:flex-row flex-col bg-foregroundPrimary"
     >
       <button
-        @click="selectedProjectCategory = service.slug"
+        @click="filterProject(service)"
         class="static-shadow-border-light px-6 py-4 text-sm font-semibold text-foreground"
-        :class="selectedProjectCategory == service ? 'bg-foregroundPrimaryHover -translate-y-1 scale-105' : 'bg-foregroundPrimary hover:bg-foregroundPrimaryp'"
-        v-for="service in services"
+        :class="
+          selectedProjectCategory == service
+            ? 'bg-foregroundPrimaryHover -translate-y-1 scale-105'
+            : 'bg-foregroundPrimary hover:bg-foregroundPrimaryHover'
+        "
+        v-for="service in [
+          'Web Development',
+          'UI/UX Design',
+          'Generative AI',
+          'DevOps',
+          'Mobile Apps Development',
+          'Cloud Computing',
+          'ERP Development',
+          'Graphics Design',
+          'All Projects',
+        ]"
       >
-        {{ service.name }}
+        {{ service }}
       </button>
     </div>
     <div class="grid md:grid-cols-2 gap-x-10 gap-y-12 mt-16">
-      <div
-        v-for="(project, index) in filteredProjects.length ? filteredProjects : projects"
-        :key="index"
-      >
-        <img :src="project.image" alt="project image" class="w-full" />
-        <div class="pt-5 flex justify-between">
-          <p class="font-semibold text-xl text-background">{{ project.title }}</p>
-          <div class="gap-x-2 gap-y-1 flex flex-wrap">
-            <button
-              v-for="(category, index) in project.categories"
-              :key="index"
-              class="bg-background text-xs text-foreground px-3 py-2 rounded-full"
-            >
-              {{ category }}
-            </button>
+      <TransitionGroup name="fade-scale">
+        <div v-for="(project, index) in filteredProjects" :key="index">
+          <img :src="project.image" alt="project image" class="w-full" />
+          <div class="pt-5 flex justify-between">
+            <p class="font-semibold text-xl text-background">{{ project.title }}</p>
+            <div class="gap-x-2 gap-y-1 flex flex-wrap">
+              <button
+                v-for="(category, index) in project.categories"
+                :key="index"
+                class="bg-background text-xs text-foreground px-3 py-2 rounded-full"
+              >
+                {{ category }}
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      </TransitionGroup>
     </div>
   </div>
 </template>
 
 <script setup>
 import projects from "@/assets/data/projects.json";
-import services from "@/assets/data/services.json";
-const selectedProjectCategory = ref("");
 
-const filteredProjects = computed(() => {
-  return projects.filter((project) =>
-    project.categories.includes(selectedProjectCategory.value)
-  );
-});
+const selectedProjectCategory = ref("All Projects");
+
+const filteredProjects = ref(projects);
+
+function filterProject(projectCategory) {
+  filteredProjects.value = [];
+  selectedProjectCategory.value = projectCategory;
+  setTimeout(() => {
+    if (selectedProjectCategory.value == "All Projects") {
+      filteredProjects.value = projects;
+    } else {
+      filteredProjects.value = projects.filter((project) =>
+        project.categories.includes(selectedProjectCategory.value)
+      );
+    }
+  }, 250);
+}
 </script>
+
+<style>
+.fade-scale-enter-active,
+.fade-scale-leave-active {
+  transition: opacity 0.5s, transform 0.5s;
+}
+.fade-scale-enter,
+.fade-scale-leave-to {
+  opacity: 0;
+  transform: scale(0.9);
+}
+</style>
